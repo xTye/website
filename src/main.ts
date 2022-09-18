3043f89ff914dab844c21d7d3d4c1d90fcfca909
xTye <iwatchtyler@gmail.com>
2022-09-18 01:58:11 -0400

    added keys

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,14 @@ import { router } from "@/router/index";
 import store from "./store";
 import { initializeApp } from "firebase/app";
 
-
 export const firebase = initializeApp({
-
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 });
 
 const app = createApp(App);
