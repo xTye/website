0b1d8f9719af55a34d330680b18ba70c656e13f5
xTye <iwatchtyler@gmail.com>
2022-09-24 03:33:49 -0400

    Redo home page and media page

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -18,15 +18,6 @@ export default defineComponent({
         </div></router-link
       >
       <router-link
-        :to="'/about'"
-        class="duration-0 hover:transition duration-300"
-        ><div
-          class="text-center p-2 w-20 h-auto hover:border-b-2 hover:bg-black hover:bg-opacity-50 transition duration-200"
-        >
-          About
-        </div></router-link
-      >
-      <router-link
         :to="'/resume'"
         class="duration-0 hover:transition duration-300"
         ><div
@@ -36,30 +27,12 @@ export default defineComponent({
         </div></router-link
       >
       <router-link
-        :to="'/pictures'"
+        :to="'/media'"
         class="duration-0 hover:transition duration-300"
         ><div
           class="text-center p-2 w-20 h-auto hover:border-b-2 hover:bg-black hover:bg-opacity-50 transition duration-200"
         >
-          Pictures
-        </div></router-link
-      >
-      <router-link
-        :to="'/videos'"
-        class="duration-0 hover:transition duration-300"
-        ><div
-          class="text-center p-2 w-20 h-auto hover:border-b-2 hover:bg-black hover:bg-opacity-50 transition duration-200"
-        >
-          Videos
-        </div></router-link
-      >
-      <router-link
-        :to="'/projects'"
-        class="duration-0 hover:transition duration-300"
-        ><div
-          class="text-center p-2 w-20 h-auto hover:border-b-2 hover:bg-black hover:bg-opacity-50 transition duration-200"
-        >
-          Projects
+          Media
         </div></router-link
       >
       <router-link
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,39 +4,81 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "HomePage",
   components: { NavigationBar },
-  data() {
-    return {
-      loaded: false,
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.loaded = true;
-    }, 1000);
-  },
 });
 </script>
 
 <template>
-  <div class="flex flex-col items-center min-h-screen bg-black text-white">
+  <div
+    class="flex flex-col items-center min-h-screen bg-black text-white overscroll-none"
+  >
     <NavigationBar />
-    <div class="flex flex-col items-center w-4/5 mt-40">
-      <div
-        :class="`${
-          !loaded ? 'translate-y-100' : ''
-        } transition ease-in-out flex justify-between`"
-      >
-        <div>Test</div>
-        <video
-          autoplay
-          loop
-          muted
-          disablePictureInPicture
-          :class="`w-3/5 h-3/5 rounded-xl drop-shadow-md`"
-        >
-          <source src="@/assets/zedmontage.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+    <div class="flex flex-col items-center">
+      <div class="flex flex-col w-9/10 my-20 items-start">
+        <div class="font-semibold text-3xl text-green">
+          Hello internet nomad,
+        </div>
+        <div class="flex flex-col gap-8 w-full text-lg text-justify">
+          <div>
+            My name is Tye and I like creating things I'm proud of. The purpose
+            of this website was to create a place where all of my media can be
+            found in one place. I have many hobbies ranging from video editing,
+            coding, game development, gaming, keyboard making, 3d printing and
+            much more.
+          </div>
+          <div>
+            <div class="text-2xl text-red">Recruiters</div>
+            <div class="flex flex-col gap-2">
+              <div>
+                I am an upcoming graduate at the University of Central Florida,
+                and I am currently looking for a respectable workplace to hone
+                my talents.
+              </div>
+              <div class="text-center">
+                <i><b>I am a critical thinker.</b></i>
+              </div>
+              <div>
+                This is something I rarely find with people I meet. I have a
+                closely knit friend group where we openly share our feelings and
+                thoughts about certain topics. These topics will range from
+                politics to fashion sense, regardless of the topic, we always
+                beg to question. I often compare our logical thinking processes
+                to the Greeks and their way of using formal logic to construct
+                arguments. Much like how I like to construct my arguments, I
+                also like to solve problems using the same systems. From
+                debugging, designing, programming, and managing to creating,
+                exploring, researching, and brainstorming I always ask
+                questions.
+              </div>
+              <div class="text-center">
+                <i><b>I am a creator.</b></i>
+              </div>
+              <div>
+                Sometimes, I go out of my way to innovate. This means making new
+                architecture for code, desgining my own graphing stencils, and
+                adding my own personal touches on video edits. In any case, I
+                always like to think outside of the box. Inventing new things is
+                what I find most exciting. Nothing bothers me more than watching
+                someone else do something and trying to replicate it. I strive
+                to impiment my own ideas and thoughts into my projects. It's
+                important to note that the style isn't always the same. My style
+                differs with my passion and preferences.
+              </div>
+              <div class="text-center">
+                <i><b>I am passionate.</b></i>
+              </div>
+              <div>
+                It's often that people take note of my passion, more than I care
+                to notice. It's not fabricated or conterfeit. It comes
+                naturally, and varies with the people I am around. Nonetheless,
+                passion alone doesn't get me everywhere. It's the hardwork and
+                dedication that drives me towards reaching my goals. I am well
+                aware that there is no magic formula to life. The reality is
+                that if you do the work, you will get the results you want. I
+                think that alone is why I am so passionate.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
